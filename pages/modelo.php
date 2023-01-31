3337408ca21412f169ad82f23deced638a842fe7
mikensothomas <thomasmikenson40@gmail.com>
2023-01-31 18:53:27 -0300

    first commit

---
+++ pages/modelo.php
@@ -0,0 +1,45 @@
+<!-- Page-header start -->
+<div class="page-header card">
+    <div class="row align-items-end">
+        <div class="col-lg-8">
+            <div class="page-header-title">
+                <div class="d-inline">
+                    <h4>Sample Page</h4>
+                    <span>lorem ipsum dolor sit amet, consectetur adipisicing elit</span>
+                </div>  
+            </div>
+        </div>
+        <div class="col-lg-4">
+            <div class="page-header-breadcrumb">
+                <ul class="breadcrumb-title">
+                    <li class="breadcrumb-item">
+                        <a href="index.php">
+                            <i class="icofont icofont-home"></i>
+                        </a>
+                    </li>
+                    <li class="breadcrumb-item"><a href="#!">Pages</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page-header end -->
+
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card">
+                <div class="card-header">
+                    <h5>Página Inicial</h5>
+                    <span>Seja bem vindo a THE NEW SCHOOL</span>
+                </div>
+                <div class="card-block">
+                    <p>
+                        Na THE NEW SCHOOL nós ensinamos vários cursos muito interessantes para você de forma acessível, prática e simples.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
